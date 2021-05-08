fd78364ed2d07f5f97bb0b1d06baf007ff971191
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2021-05-08 23:51:18 +0200

    Added command line parsing for site creation
    Initial argument parsing requiring a site name, output (working)
    directory, and the list of cameras.

--- create_cam_site.py
+++ create_cam_site.py
@@ -1,13 +1,39 @@
 """Main file to create a static cam site page.
 
+The script expects a prepared directory with images and thumbnails as produced by the
+create_cam_site.py script.
+
 Usage:
-  create_cam_site
+  create_cam_site.py --name "My CamSite" --dir ./out --cameras camera-front camera-back
 """
 from os import path
-from typing import Sequence
+from typing import Sequence, List
+
+from tap import Tap
 
 from cam_file import load_cam_data, IMAGES_DIRECTORY
 from cam_site_builder import CamSiteBuilder
+
+
+class CreateCamArgumentParser(Tap):
+    name: str  # Cam site name
+    """The title of the cam site as used in the created website.
+    """
+    dir: str  # Output directory
+    """The directory that will contain the created cam site. Also used as input
+    directory for `images` and `thumbnails`. This is the same output directory
+    used for the preparation script.
+    """
+    cameras: List[str]  # Cameras in the cam site.
+    """The cameras are directory names in the `images` and `thumbnails`
+    directories. They are the same as used as input for the preparation script.
+    """
+
+
+def _get_arguments() -> CreateCamArgumentParser:
+    parser = CreateCamArgumentParser()
+    parsed_args = parser.parse_args()
+    return parsed_args
 
 
 def create_cam_site(project_name: str, web_root: str, cameras: Sequence[str]):
@@ -26,3 +52,8 @@ def create_cam_site(project_name: str, web_root: str, cameras: Sequence[str]):
     builder = CamSiteBuilder(project_name, web_root, cam_data)
 
     builder.create_main_site()
+
+
+if __name__ == '__main__':
+    args = _get_arguments()
+    create_cam_site(args.name, args.dir, args.cameras)
